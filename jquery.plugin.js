e09ab644c8487fec4db2eed1e836c496474a1862
Chris <chris@1337.af>
2012-07-17 00:15:21 +0000

    initial plugin file commit

---
+++ jquery.plugin.js
@@ -0,0 +1,61 @@
+/* jQuery plugin Template
+ * 
+ * Licensed under the MIT:
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright (C) 2012 by Chris Landa chris[-at-]1337[-dot-]af
+ */
+
+(function($, jQuery){
+  var methods = {
+    init: function(options){
+      var settings = {
+	'var1': 'value'
+      };
+
+      return this.each(function(){
+	var $this = $(this);
+
+	if(options){
+	  $.extend(settings, options);
+	}
+
+	console.log('This is the init method. The value of var1 is: ' + settings['test']);
+	privateFunc();
+      })
+    },
+
+    reset: function(){
+      return this.each(function(){
+	var $this = $(this);
+
+	console.log('This is the reset method');
+      })
+    },
+
+    destroy: function(){
+      return this.each(function(){
+	var $this = $(this);
+
+	console.log('This is the destroy method');
+      })
+    }
+  };     
+
+  $.fn.myPlugin = function(method){
+    if(methods[method]){
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else if(typeof method === 'object' || !method){
+      return methods.init.apply(this, arguments);
+    }
+    else{
+      $.error('Method ' +  method + ' does not exist on jQuery.myPlugin');
+    }
+  };
+  
+  var privateFunc = function(){
+    console.log('This is a private function!');
+  }
+  
+})(jQuery, jQuery);
